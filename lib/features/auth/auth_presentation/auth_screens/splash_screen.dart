9d14a403a679439cd3bd8a70e6495ec562f79910
Steve Amgad <steve.amgad.005@gmail.com>
2026-03-13 22:38:43 +0200

    splash Screen

---
+++ lib/features/auth/auth_presentation/auth_screens/splash_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../auth_widgets/app_background.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AppBackground(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(child: Image.asset("images/logo.png")),
+            SizedBox(height: 20),
+            MaterialButton(
+              onPressed: () {},
+              color: Color(0xFFC9B59C),
+              minWidth: 223,
+              height: 59,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Text(
+                "Login",
+                style: TextStyle(color: Color(0xFF6B6B6B), fontSize: 36,fontWeight: FontWeight.bold),
+              ),
+            ),SizedBox(height: 24,),MaterialButton(
+              onPressed: () {},
+              color: Color(0xFFC9B59C),
+              minWidth: 223,
+              height: 59,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Text(
+                "Sign Up",
+                style: TextStyle(color: Color(0xFF6B6B6B), fontSize: 36,fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/auth/auth_presentation/auth_widgets/app_background.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppBackground extends StatelessWidget {
+  final Widget child;
+
+  const AppBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Color(0xFFEFE9E3), Color(0xFFD9CFC7)],
+        ),
+      ),
+      child: child,
+    );
+  }
+}
